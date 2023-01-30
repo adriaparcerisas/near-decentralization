8c54775ff33f32fc827f526d428b312d9066b3ed
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-01-30 12:57:07 +0100

    Update Near_Decentralization.py

--- Near_Decentralization.py
+++ Near_Decentralization.py
@@ -62,7 +62,7 @@ options = ['All', 'Figment', 'Astro-Stakers','Near-Fans','Blockdaemon','Stake1',
 label = "<p style='color:red; font-size:25px'>Choose a validator</p>"
 selected_option = st.selectbox(label, options, format_func=lambda x: f"<p style='color:blue; font-size:20px'>{x}</p>")
 st.write('You selected:', selected_option)
-st.markdown('_Please consider that it could take a while (up to 2 minute) to load all charts if you selected a different option._'
+st.markdown('_Please consider that it could take a while (up to 2 minute) to load all charts if you selected a different option._')
 #st.sidebar.selectbox('Choose a validator', options)
 
 
